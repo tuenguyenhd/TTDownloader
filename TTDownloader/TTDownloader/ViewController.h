48e98fbea8f1787ce0c3e73654b5721288a1cfa0
Tue Nguyen <tuent@appota.com>
2015-09-15 09:44:32 +0700

    Initial project - pod

---
+++ TTDownloader/TTDownloader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TTDownloader
+//
+//  Created by Tue Nguyen on 9/15/15.
+//  Copyright (c) 2015 tuent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TTDownloader/TTDownloader/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TTDownloader
+//
+//  Created by Tue Nguyen on 9/15/15.
+//  Copyright (c) 2015 tuent. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
